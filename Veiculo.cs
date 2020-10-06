4dc479511850d097418008b48be02dfbbf5e62b5
Daniel da Silva <danieldasilva@ucl.br>
2020-10-06 00:32:14 +0000

    Veículo

--- Veiculo.cs
+++ Veiculo.cs
@@ -4,7 +4,7 @@ class Atributos_do_Veiculo{
  private string marca;
  private int ano;
  private double valor;
- public int km;
+ private int km;
  
  public void Setmarca(string m){
     marca = m;
@@ -15,7 +15,9 @@ class Atributos_do_Veiculo{
   public void Setvalor(double v){
     valor= v;
   }
-
+  public void Setkm(int k){
+    km = k;
+  }
 public void relatório(string marca, int ano, double valor, int km){
   Console.WriteLine("Marca Escolhida> {0} - Ano >> {1} - Valor >> {2} Reais - Kilometragem >> {3}", marca, ano, valor, km);
 
