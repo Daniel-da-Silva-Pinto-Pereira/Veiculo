a0e723344501513f247b693f6b01417089c355e6
Daniel da Silva <danieldasilva@ucl.br>
2020-10-06 00:28:41 +0000

    Veiculo

--- Vendedor.cs
+++ Vendedor.cs
@@ -4,8 +4,11 @@ class vendedor_de_veiculos{
 private double salário;
 public double comissão;
 
-public receber_salario(double salário, double comissão, double valor_das_vendas){
-return salário+comissão*valor_das_vendas;
+public void Setsalário(double s){
+    salário = s;
+  }
+public double receber_salario(double salário, double comissão, double valor_das_vendas){
+return  salário+comissão*valor_das_vendas;
 
 }
 }
